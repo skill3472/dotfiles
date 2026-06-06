5f89bfc1025f1cc2a3b29886f1e04df9ea5cb782
Maksymilian Tym <social@mtym.me>
2026-06-06 11:48:24 +0200

    Switched to sway

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,22 @@
-if status is-interactive
+if test (tty) = "/dev/tty1"
+	exec sway
+end
+
+if status is-interactive && not set -q TMUX
 	# Vars
-	export EDITOR=vim
+	export EDITOR=nvim
 	export hydro_color_pwd=green
+	export PATH=/usr/bin/:/opt/jdk-18.0.2/bin/:/usr/bin/idea/bin/:/home/skill/.local/bin/
+
+	tmux new-session -A -s main
+	kill $KITTY_PID
+end
+
+function y
+    set tmp (mktemp -t yazi-cwd.XXXXX)
+    yazi $argv --cwd-file=$tmp
+    if set cwd (cat -- $tmp); and test -n "$cwd"; and test "$cwd" != "$PWD"
+        cd -- $cwd
+    end
+    rm -f -- $tmp
 end
--- fish/.config/fish/functions/ls.fish
+++
@@ -1,4 +0,0 @@
-function ls --wraps='exa -al --color=always --group-directories-first' --wraps='exa --color=always --group-directories-first' --description 'alias ls=exa --color=always --group-directories-first'
-  exa --color=always --group-directories-first $argv
-        
-end
